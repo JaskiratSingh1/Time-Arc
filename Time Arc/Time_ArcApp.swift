d8b6b1f7d63f9bf1d6558e13eae4ea297d304ab2
Jaskirat Singh <38223354+JaskiratSingh1@users.noreply.github.com>
2025-07-29 10:02:33 -0600

    Initial Commit

---
+++ Time Arc/Time_ArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_ArcApp.swift
+//  Time Arc
+//
+//  Created by Jaskirat Singh on 2025-07-29.
+//
+
+import SwiftUI
+
+@main
+struct Time_ArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
